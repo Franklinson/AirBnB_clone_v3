9addf4e339e0794aa7b7fdf3b74006f1d160eae5
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-10-01 23:23:55 -0700

    minor edits

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,8 @@ from flask import Flask, jsonify, request, abort
 from models import storage
 from models.place import Place
 from api.v1.views import app_views
+from models.city import City
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places' strict_slashes=False)
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,6 +6,8 @@ from flask import Flask, jsonify, request, abort
 from models import storage
 from models.review import Review
 from api.v1.views import app_views
+from models.place import Place
+from models.user import User
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
